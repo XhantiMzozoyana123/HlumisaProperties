a1980e0668aa843d4da64b294df30a677b7f1239
Xhanti Mzozoyana <xhantimzozoyana123@gmail.com>
2026-06-26 18:06:58 +0200

    Changed ValueController.

--- HlumisaProperties.Api/Controllers/ValuesController.cs
+++ HlumisaProperties.Api/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace HlumisaProperties.Api.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value11", "value22" };
         }
 
         // GET api/<ValuesController>/5
